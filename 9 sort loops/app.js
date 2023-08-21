fd29b701e3926c252fd9ee6680ea1c6d18d7b5fe
Eugene <smilefox2806@gmail.com>
2023-08-21 19:42:14 +0700

    Исправил с учетом комментариев

--- 9 sort loops/app.js
+++ 9 sort loops/app.js
@@ -1,29 +1,15 @@
-const arr = [-1, 0, 11, 2, 202, 4, 21, 6, 3, 76, 8, -12];
+const arr = [-1, 0, 11, 2, 202, 4, 21, 6, 3, 76, 8, -12, 22, 13, 47];
 
-function sort(array) {
-    const arrSort = [];
-    let bufer;
-    for (let i = array.length - 1; i > 0 ; i--) {
-        bufer = array[i] - 1;
-        for (let j = 0; j < array.length; j++) {
-            if (array[i] > array[j] && array[i] > bufer) {
-                bufer = array[i];
-            } 
-             if (array[j] > bufer) {
-                bufer = array[j];
-            }   
+function sortArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
         }
-        indexRemoveElement = arr.indexOf(bufer);
-        arr.splice(indexRemoveElement, 1);
-        arrSort.unshift(bufer);
-        bufer = 0;
     }
-    if (array.length === 1) {
-        const lastElement = arr.splice(0, 1);
-        arrSort.unshift(lastElement[0]);
-    }
-    return arrSort;
+    return arr;
 }
- 
+
 console.log(`Исходный массив => ${arr}`);
-console.log(`Отсортированный массив => ${sort(arr).join(', ')}`);
+console.log(`Отсортированный массив => ${sortArray(arr).join(', ')}`);
