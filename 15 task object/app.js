55f67abbaef70db2374c96e9595f67fff714bcda
Eugene <smilefox2806@gmail.com>
2023-09-11 14:59:22 +0700

    Исправил update и add функции

--- 15 task object/app.js
+++ 15 task object/app.js
@@ -6,15 +6,13 @@ const toDoList = {
       priority: 1,
       id: 1 },
     ],
+    lastId: 1,
    
     addTask(task) {
+        task = {...task, id: this.lastId+1};
         this.tasks.push(task);
-        // const findTaskID = this.tasks.findIndex(task => task.id === id);
-        // if (findTaskID === -1) {
-        //     this.tasks.push({ title, id, priority });
-        // } else {
-        //     console.log(`Задача с ID ${id} уже создана`);
-        // };
+        this.lastId = this.lastId + 1;
+        
     },
     removeTask(id) {
         const removeTaskIndex = this.tasks.findIndex(task => task.id === id);
@@ -25,12 +23,12 @@ const toDoList = {
          };
          
     },
-    updateTask(id, updatedTask) {
-        const findTaskID = this.tasks.findIndex(task => task.id === id);
+    updateTask(updatedTask) {
+        const findTaskID = this.tasks.findIndex(task => task.id === updatedTask.id);
         if (findTaskID !== -1) {
-            this.tasks[findTaskID] = {...updatedTask, id};
+            this.tasks[findTaskID] = {...this.tasks[findTaskID], ...updatedTask};
         } else {
-            console.log(`Задача с ID ${id} не найдена`);
+            console.log(`Задача с ID ${updatedTask.id} не найдена`);
         };
     },
     sordByPriority(parameter = 'asc') {
@@ -50,42 +48,78 @@ const toDoList = {
 toDoList.addTask({
     title: 'Купить продукты',
     priority: 3,
-    id: toDoList.tasks.length + 1
+    
 });
 toDoList.addTask({
     title: 'Прочитать книгу',
     priority: 5,
-    id: toDoList.tasks.length + 1
+    
 });
 toDoList.addTask({
     title: 'Пропылесосить',
     priority: 4,
-    id: toDoList.tasks.length + 1
+    
 });
-console.log(toDoList.tasks);
-toDoList.tasks.forEach(task => {
-    console.log(task);
-});
-console.log('');
 
-toDoList.removeTask(2);
-console.log(toDoList.tasks);
 toDoList.tasks.forEach(task => {
     console.log(task);
 });
-console.log('');
+console.log('Добавление задач');
 
-toDoList.updateTask(3, {title: 'Составить план на день', priority: 8});
-console.log(toDoList.tasks);
+toDoList.removeTask(1);
 toDoList.tasks.forEach(task => {
     console.log(task);
 });
-console.log('');
+console.log('Удаление задачи 1');
+
+toDoList.updateTask({title: 'Составить план на день', priority: 3, id: 3});
+toDoList.tasks.forEach(task => {
+    console.log(task);
+});
+console.log('Обновление задачи 3');
 
 toDoList.sordByPriority();
-console.log(toDoList.tasks);
 toDoList.tasks.forEach(task => {
     console.log(task);
 });
+console.log('Сортировка задач');
+toDoList.addTask({
+    title: 'Купить продукты',
+    priority: 2,
+   
+});
+
+toDoList.tasks.forEach(task => {
+    console.log(task);
+});
+console.log('добавление задачи');
+
+toDoList.addTask({
+    title: 'Помыть машину',
+    priority: 5,
+    
+});
+
+toDoList.tasks.forEach(task => {
+    console.log(task);
+});
+console.log('Добавление задачи');
 
 
+toDoList.removeTask(3);
+
+toDoList.tasks.forEach(task => {
+    console.log(task);
+});
+console.log('Удаление задачи 3');
+
+toDoList.addTask({
+    title: 'Купить тетрадь',
+    priority: 7,
+    
+});
+
+toDoList.tasks.forEach(task => {
+    console.log(task);
+});
+console.log('Добавление задачи');
