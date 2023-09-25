7fa00e96cd3bcc72e31da5e4169ad3ddec41e242
Eugene <smilefox2806@gmail.com>
2023-09-25 21:47:13 +0700

    Исправил ошибки

--- 17 calc/app.js
+++ 17 calc/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const textPanel = document.querySelector('.panel_text_hidden');
+
 document.querySelectorAll('.input').forEach(el => {
     el.addEventListener('click', () => {
-        document.querySelector('.panel_text_hidden').classList.remove('panel_text_active');
-        document.querySelector('.panel_text_hidden').innerText = '';
+        textPanel.classList.remove('panel_text_active');
+        textPanel.innerText = '';
     });
 });
 
@@ -23,11 +25,29 @@ function divide(a, b) {
     return a / b;
 }
 
+document.querySelector('.buttons').addEventListener('click', event => {
+    if(event.target.className === 'button') {
+        switch(event.target.id) {
+            case 'addition': calculation(addition);
+                break;
+            case 'subtraction': calculation(subtraction);
+                break;
+            case 'multiply': calculation(multiply);
+                break;
+            case 'divide': calculation(divide);
+                break;
+            case 'equal': equal();
+                break;
+            default: console.log('Что-то пошло не так');
+        }
+    }
+});
+
 function calculation(fn) {
     const a = Number(document.querySelector('.input_1').value);
     const b = Number(document.querySelector('.input_2').value);
     if(a && b) {
-         document.querySelector('.panel_text_hidden').innerText =  fn(a, b);
+        textPanel.innerText =  (fn(a, b)).toFixed(6);
     }
     return;
 }
@@ -35,5 +55,5 @@ function calculation(fn) {
 function equal() {
     document.querySelector('.input_1').value = '';
     document.querySelector('.input_2').value = '';
-    document.querySelector('.panel_text_hidden').classList.add('panel_text_active');
+    textPanel.classList.add('panel_text_active');
 }
